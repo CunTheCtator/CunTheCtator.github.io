3057373b0633e87ec4ecbf08bce4c661a0a99700
CunTheCtator <maxim.murashkovsky@gmail.com>
2023-09-02 15:18:38 +0300

    New way of displaying posts

--- scripts.js
+++ scripts.js
@@ -9,101 +9,132 @@ var firebaseConfig = {
     measurementId: "G-JCQZEXKZP3"
   };
   
-  // Initialize Firebase with your configuration
+// Initialize Firebase with your configuration
 firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the Firestore database
 const db = firebase.firestore();
 
-// Define a function to create a new blog post
+// Function to create a new blog post (for create.html)
 function createBlogPost(title, content) {
     // Ensure title and content are valid strings
     if (typeof title !== "string" || typeof content !== "string") {
         console.error("Invalid data types for title or content.");
         return; // Exit the function if data types are invalid
     }
-    console.log("createBlogPost function called");
+
     // Add a new document to the "blogposts" collection
     db.collection("blogposts")
         .add({
-        title: title,
-        content: content,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+
+            title: title,
+            content: content,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         })
         .then((docRef) => {
-        // Document added successfully
-        console.log("Document written with ID: ", docRef.id);
-    })
+            // Document added successfully
+            console.log("Document written with ID: ", docRef.id);
+        })
         .catch((error) => {
-        console.error("Error adding document: ", error);
+            console.error("Error adding document: ", error);
         });
 }
 
-// Function to display blog posts
+// Function to display blog posts (for index.html)
 function displayBlogPosts() {
-  const blogList = document.getElementById("blog-list");
+    const blogList = document.getElementById("blog-list");
 
-  // Clear existing posts
-  blogList.innerHTML = "";
+    // Clear existing posts
+    blogList.innerHTML = "";
 
-  // Retrieve and display blog posts
-  db.collection("blogposts")
-    .orderBy("timestamp", "desc")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        const article = document.createElement("article");
-        const titleElement = document.createElement("h2");
-        const contentElement = document.createElement("p");
+    // Retrieve and display blog posts
+    db.collection("blogposts")
+        .orderBy("timestamp", "desc")
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                const article = document.createElement("article");
+                const titleElement = document.createElement("h2");
+                const contentElement = document.createElement("p");
 
-        titleElement.textContent = data.title;
-        contentElement.textContent = data.content;
+                titleElement.textContent = data.title;
+                contentElement.textContent = data.content;
 
-        article.appendChild(titleElement);
-        article.appendChild(contentElement);
+                article.appendChild(titleElement);
+                article.appendChild(contentElement);
 
-        blogList.appendChild(article);
-      });
-    })
-    .catch((error) => {
-      console.error("Error getting documents: ", error);
-    });
+                // Create a link to the individual blog post
+                const link = document.createElement("a");
+                link.href = `individual-blog.html?id=${doc.id}`;
+                link.textContent = "Read More";
+
+                article.appendChild(link);
+
+                blogList.appendChild(article);
+            });
+        })
+        .catch((error) => {
+            console.error("Error getting documents: ", error);
+        });
 }
 
-// Call the displayBlogPosts function to initially load blog posts
+// Call the displayBlogPosts function to initially load blog posts (for index.html)
 if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
-    displayBlogPosts()
+    displayBlogPosts();
 }
 
-// Handle the form submission
+// Handle the form submission (for create.html)
 document.addEventListener("DOMContentLoaded", function () {
     // Check if the current page is create.html before handling form submission
     if (window.location.pathname.includes("create.html")) {
-      // Handle form submission for create.html
-      document.getElementById("blog-form").addEventListener("submit", function (e) {
-        e.preventDefault();
-  
-        // Get the values from the form
-        const title = document.getElementById("title").value;
-        const content = document.getElementById("content").value;
-  
-        // Create a new blog post using the provided values
-        createBlogPost(title, content)
-            // Callback function to execute after the blog is added to Firebase
+        // Handle form submission for create.html
+        document.getElementById("blog-form").addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            // Get the values from the form
+            const title = document.getElementById("title").value;
+            const content = document.getElementById("content").value;
+
+            // Create a new blog post using the provided values
+            createBlogPost(title, content);
+
             // Clear the form inputs
-        document.getElementById("title").value = "";
-        document.getElementById("content").value = "";
-        
-        // Redirect to the index.html page after creating the post
-        setTimeout(() => {
-            window.location.href = "index.html"; // Update the URL to match your file structure
-        }, 1500); // Adjust the timeout delay as needed
-      });
+            document.getElementById("title").value = "";
+            document.getElementById("content").value = "";
+
+            // Redirect to the index.html page after creating the post
+            setTimeout(() => {
+                window.location.href = "index.html"; // Update the URL to match your file structure
+            }, 1500); // Adjust the timeout delay as needed
+        });
     }
-  
-    // Check if the current page is index.html and then call displayBlogPosts
-    if (window.location.pathname.includes("index.html")) {
-      displayBlogPosts();
+
+    // Check if the current page is individual-blog.html and then display the individual blog post
+    if (window.location.pathname.includes("individual-blog.html")) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const blogId = urlParams.get("id");
+
+        if (blogId) {
+            // Retrieve the individual blog post
+            db.collection("blogposts")
+                .doc(blogId)
+                .get()
+                .then((doc) => {
+                    if (doc.exists) {
+                        const data = doc.data();
+                        const titleElement = document.getElementById("individual-blog-title");
+                        const contentElement = document.getElementById("individual-blog-content");
+
+                        titleElement.textContent = data.title;
+                        contentElement.textContent = data.content;
+                    } else {
+                        console.error("No such document!");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error getting document:", error);
+                });
+        }
     }
 });
