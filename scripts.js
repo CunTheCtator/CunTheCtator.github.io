f5d81e20c951bae608dad322f86a2537cb643e4d
CunTheCtator <maxim.murashkovsky@gmail.com>
2023-09-02 15:44:32 +0300

    reverted back, because buuuuuuuuuuuuuuuuuuuuuuuugs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 // Import the Firebase configuration
-import firebaseConfig from './firebase-config.js';
+var firebaseConfig = {
+  apiKey: "AIzaSyBoitfZcjc0hw2bXaDbbbVx-TI5bgnMPqw",
+  authDomain: "testwebsite-d9b31.firebaseapp.com",
+  projectId: "testwebsite-d9b31",
+  storageBucket: "testwebsite-d9b31.appspot.com",
+  messagingSenderId: "337215100645",
+  appId: "1:337215100645:web:b7d38ae5c563579e33260c",
+  measurementId: "G-JCQZEXKZP3"
+}
 
 // Initialize Firebase with the imported configuration
 firebase.initializeApp(firebaseConfig);
